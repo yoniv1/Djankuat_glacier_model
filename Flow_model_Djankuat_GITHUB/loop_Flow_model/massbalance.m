1b9477013c7b0f612fbafa316551576533f6fe2f
yoniv1 <48556227+yoniv1@users.noreply.github.com>
2020-10-09 12:31:44 +0200

    Update massbalance.m

--- Flow_model_Djankuat_GITHUB/loop_Flow_model/massbalance.m
+++ Flow_model_Djankuat_GITHUB/loop_Flow_model/massbalance.m
@@ -3,34 +3,24 @@
 % ====================================================================
 
 % --------------------------------------------------------------------
-% Piecewise linear mass balance profile mimicking the observed 
+% Mass balance profile 
 % --------------------------------------------------------------------
 
-% Parameterize ablation and accumulation gradients
+% Mass balance (m y^-1 w.e. m^-1)
 
-acc_prof = 0.00250*(sur_obs-ELA) + 2.52444;
-acc_prof(1:nelh) = 0.00140*(sur_obs(1:nelh)-ELA) + 2.59444;
-abl_prof = -0.00550*(sur_obs-ELA) + 2.66776;
-abl_prof(1:nelh) = -0.00474*(sur_obs(1:nelh)-ELA) + 2.66776;
+balweh = betah2*(sur_obs-ELA);
+balweh(1:nelh) = betah1*(sur_obs(1:nelh)-ELA); 
 
-mb_prof = acc_prof-abl_prof;
-balweh = mb_prof;
-
-% Adjust for debris
+%% Adjust for debris
 
 if time > 1
     
-% Parameterize amount of run-off (RO) that is ice melt (M_ice)
+% Parameterize amount that is ice melt (M_ice)
 
-fract_ice = min(-0.001659*(sur_obs-ELA) - 0.021334,1);
-fract_ice(1:nelh) = 0;
-fract_ice = max(fract_ice,0);
+abl_ice = zeros(size(balweh));
 
-abl_ice = abl_prof.*(fract_ice);
-
-% Amount of run-off (RO) that is meltwater outflow from snowpack (W_snow)
-
-abl_snow = abl_prof.*(1-fract_ice);
+abl_ice(1:nelh) = 0;
+abl_ice(nelh+1:end) = balweh(nelh+1:end);
 
 % Calculate fdebris
 
@@ -42,13 +32,13 @@ debrismelt_debris = abl_ice.*(transpose(debrisarea_d_past(time-1,:))).*(transpos
 debrismelt_debrisfree = abl_ice.*(transpose(1-debrisarea_d_past(time-1,:)));
 debrismelt = debrismelt_debris + debrismelt_debrisfree;
 
-% New ablation profile
-
-abl_prof_new = abl_snow + debrismelt;
-
 % New mass balance profile
 
-mb_prof_new = acc_prof-abl_prof_new;
+mb_prof_new = zeros(size(balweh));
+
+mb_prof_new(1:nelh) = balweh(1:nelh);
+mb_prof_new(nelh+1:end) = debrismelt(nelh+1:end);
+
 balweh = mb_prof_new;
 
 end
