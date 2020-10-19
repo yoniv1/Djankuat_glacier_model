e1c32b0c61700372e4a2ec8c8e62d01320415d29
yoniv1 <48556227+yoniv1@users.noreply.github.com>
2020-10-19 15:42:32 +0200

    Update debris_input.m

--- Flow_model_Djankuat_GITHUB/loop_Debris_model/debris_model/debris_input.m
+++ Flow_model_Djankuat_GITHUB/loop_Debris_model/debris_model/debris_input.m
@@ -8,14 +8,11 @@ inoutdebris = 0;
 
 % Debris input along flow line
 
-if time >= tdebris && yearly_wsfc(time,inputlocation_debris) > wsfc_xinput_tdebris*width_shrinkage
-    
 inoutdebris(inputlocation_debris) = (depositionrate_debris);
 
-end
-
-% Debris output at terminus
+% Debris output at front
 
 inoutdebris(leng./deltax_d) = (((terminusflux_cst).*(balweh(leng./deltax_d).*h_debris(leng./deltax_d)))./(1));
 
 inoutdebris((leng./deltax_d)+1) = (-inoutdebris(leng./deltax_d)); 
+
