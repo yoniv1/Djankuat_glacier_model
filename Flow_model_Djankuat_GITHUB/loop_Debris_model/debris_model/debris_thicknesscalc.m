cf99932eb2195f64041d9222797d670fe4097cde
yoniv1 <48556227+yoniv1@users.noreply.github.com>
2020-10-19 15:43:21 +0200

    Update debris_thicknesscalc.m

--- Flow_model_Djankuat_GITHUB/loop_Debris_model/debris_model/debris_thicknesscalc.m
+++ Flow_model_Djankuat_GITHUB/loop_Debris_model/debris_model/debris_thicknesscalc.m
@@ -2,13 +2,17 @@
 % Calculation of final term for prognostic debris thickness change
 % ---------------------------------------------------------------------
 
+% ---------------------------------------------------------------------
+% Calculation of final term for prognostic debris thickness change
+% ---------------------------------------------------------------------
+
 for i=2:xnum_d
     
  % Initialize
     
  	 h_debrisini(i) = h_debris(i);
 
-% Calculate for glacier profile except terminus
+% Calculate for glacier profile 
 
 	if i < (leng./deltax_d)
   
@@ -34,15 +38,15 @@ end
 
 	if i == (leng./deltax_d);
   	
-    % Calcualte flux gradient on normal grid points
+% Calcualte flux gradient on normal grid points
 
  	 term1_debris(i) = -((fl_debris(i)-fl_debris(i-1))/(deltax_d));
   
-   	 % Meltout of debris material from the ice
+  	% Meltout of debris material from the ice
   
  	 term2_debris(i) = meltout_debris(i);
   
- 	 % Terminus wasting
+ 	 % In and output
   
  	 term3_debris(i) = inoutdebris(i);
 	  
@@ -52,26 +56,26 @@ end
 
     end
     
-% Calculate for first foreland point
-    
-    if i == ((leng./deltax_d)+1)
-        
-     % Calcualte flux gradient on normal grid points
+    % Calculate for first foreland point
 
- 	 term1_debris(i) = 0;
+	if i == ((leng./deltax_d)+1);
+  	
+% Calcualte flux gradient on normal grid points
+
+ 	 term1_debris(i) = -((fl_debris(i)-fl_debris(i-1))/(deltax_d));
   
-   	 % Meltout of debris material from the ice
+  	% Meltout of debris material from the ice
   
- 	 term2_debris(i) = 0;
+ 	 term2_debris(i) = meltout_debris(i);
   
- 	 % Terminus wasting
+ 	 % In and output
   
  	 term3_debris(i) = inoutdebris(i);
 	  
  	 % Calculate new debris thickness
   
   	 h_debris(i) = h_debrisini(i) + deltat_d*(term1_debris(i) - term2_debris(i) + term3_debris(i));
-    
+
     end
 
   	% Debris thickness to 0 if negative
